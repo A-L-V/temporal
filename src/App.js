734ab910a947f62e845cbaf27bc5a2ce4f75c3c4
ALV <richardalvaradoflores@gmail.com>
2023-03-06 15:17:20 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <Link to="/">
+            <img src='https://plazavea.vteximg.com.br/arquivos/LogoPlazaVea.svg' alt='imagen'/>            
+
+          </Link>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">Cart</a>
+           
+            <div className="dropdown">
+            <Link to="/products">Products</Link>
+              <ul className="dropdown-content">
+                <li>
+                  <Link to="/products">Products</Link>
+                </li>
+              </ul>
+            </div>
+        </div>
       </header>
+      <main className="main">
+        <div className="content">
+          <Routes>
+            <Route path="/products"  />
+            <Route path="/" exact  />
+          </Routes>
+
+        </div>
+      </main>
+      <footer className="footer">All right reserved.</footer>
     </div>
+  </BrowserRouter>
   );
 }
 

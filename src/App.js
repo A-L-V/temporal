774d8311bd28299210d9330b0a30cad94938479f
ALV <richardalvaradoflores@gmail.com>
2023-03-06 15:50:40 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
-import './index.css';
+import './App.css';
 import HomeScreen from './screens/HomeScreen';
+import ProductsScreen from './screens/ProductsScreen';
 function App() {
+
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <Link to="/">
-            <img src='https://plazavea.vteximg.com.br/arquivos/LogoPlazaVea.svg' alt='imagen'/>            
-          </Link>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-           
-            <div className="dropdown">
-            <Link to="/products">Products</Link>
-              <ul className="dropdown-content">
-                <li>
-                  <Link to="/products">Products</Link>
-                </li>
-              </ul>
-            </div>
-        </div>
-      </header>
-      <main className="main">
-        <div className="content">
-        <Routes>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <Link to="/">
+              <img src='https://plazavea.vteximg.com.br/arquivos/LogoPlazaVea.svg' alt='imagen'/>            
+            </Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>  
+              <div className="dropdown">
+              <Link to="/products">Products</Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/products">Products</Link>
+                  </li>
+                </ul>
+              </div>
+          </div>
+        </header>
+        <main className="main">
+          <div className="content">
+            <Routes>
+              <Route path="/products" element={<ProductsScreen />} />
               <Route path="/" exact element={<HomeScreen />} />
             </Routes>
-
-        </div>
-      </main>
-      <footer className="footer">All right reserved.</footer>
-    </div>
-  </BrowserRouter>
+          </div>
+        </main>
+        <footer className="footer">All right reserveda.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
